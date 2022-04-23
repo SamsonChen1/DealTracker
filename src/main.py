2bd727b958dccd1779305105c91cd309029da7b9
SamsonChen1 <samsonchen@aol.com>
2022-04-23 00:43:15 -0400

    Updating the gitignore file

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 # Main class
-from datetime import time
+import time
 from selenium import webdriver
 
 from navigation.common_navigation import CommonNavigation
@@ -7,7 +7,8 @@ from navigation.common_navigation import CommonNavigation
 def main():
     nav = CommonNavigation("https://github.com/")
     driver = nav.get_driver()
-    driver.exit()
+    nav.change_url(driver, "https://www.amazon.com/")
+    nav.exit_browser(driver)
 
 if __name__ == "__main__":
     main()
--- src/navigation/common_navigation.py
+++ src/navigation/common_navigation.py
@@ -5,7 +5,15 @@ class CommonNavigation:
         self.url = url
 
     def get_driver(self):
-        driver = webdriver.Chrome("C:\Drivers\chromedriver_v93.exe")
+        driver = webdriver.Chrome("C:\Driver\chromedriver.exe")
         driver.set_window_size(1600, 900)
         driver.get(self.url)
         return driver
+
+    def change_url(self, driver, new_url):
+        self.url = new_url
+        driver.get(self.url)
+        return driver
+
+    def exit_browser(self, driver):
+        driver.quit()
